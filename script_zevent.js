0319962849e2e40e8b911b3247ada121097554f0
Shiluryu <72858433+Shiluryu@users.noreply.github.com>
2022-04-04 10:44:02 +0200

    Update script_zevent.js

--- script_zevent.js
+++ script_zevent.js
@@ -13,12 +13,12 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://github.com/Shiluryu/ZeventRedditPixel/blob/main/zevent_picture_pixel.png";
+            i.src = "https://raw.githubusercontent.com/Shiluryu/ZeventRedditPixel/main/zevent_picture_pixel.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
             console.log(i);
             return i;
         })())
- 
+
     }, false);
- 
+
 }
