7be7b7367b74069ad182c193e952563b006a7955
Shiluryu <72858433+Shiluryu@users.noreply.github.com>
2022-04-04 10:36:04 +0200

    Update and rename script_user.js to script_zevent.js

--- script_zevent.js
+++ script_zevent.js
@@ -13,7 +13,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://i.imgur.com/6vh0IX6.png";
+            i.src = "https://github.com/Shiluryu/ZeventRedditPixel/blob/main/zevent_picture_pixel.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
             console.log(i);
             return i;
